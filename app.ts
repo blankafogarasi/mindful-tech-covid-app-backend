cd114c4c1ef33aefcd1fd5bb6c409859dd4c7327
Blanka <blankafogarasi@gmail.com>
2021-12-29 14:08:24 +0100

    minor changes after deadline

--- app.ts
+++ app.ts
@@ -3,12 +3,17 @@ import express from "express";
 import * as bodyParser from "body-parser";
 import {router} from "./src/routes/routes";
 import { Cron } from "./src/cron"
+import * as apiManager from "./src/models/apiModel";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
 app.use("/stats", router);
+
+(async () => {
+    await apiManager.saveCovidStats();
+})();
 
 new Cron();
 
